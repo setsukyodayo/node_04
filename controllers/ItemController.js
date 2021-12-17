f06c906e07580377c3512944c04418992fbae233
setsukyodayo <200098ve@yse-c.net>
2021-12-17 09:08:37 +0900

    a

--- controllers/ItemController.js
+++ controllers/ItemController.js
@@ -1,13 +1,13 @@
 const item = require('../models/item')
-exports.show= (req, res) => {
+exports.show = (req, res) => {
     const id = req.params.id
     console.log(id)
     let data = {}
     data.item = item.find(id)
     res.render('item/show.ejs', data)
 }
-exports.index=(req, res) => {
-    let data={}
+exports.index = (req, res) => {
+    let data = {}
     data.item = item.values
     res.render('item/index.ejs', data)
 }
